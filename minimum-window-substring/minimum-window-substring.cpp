4f0e63ba5d7173ee0f37cb64b95a5c591ac267b8
Mridul Sharma <106507655+engineer-br0@users.noreply.github.com>
2023-05-09 15:37:36 +0530

    Time: 59 ms, Memory: 8.3 MB - LeetHub

---
+++ minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        map<char, int> mpt;
+        map<char, int> mps;
+        int count = t.size();  // 
+        int n = s.size(); 
+        for(int i =0; i<count; i++){
+            mpt[t[i]]++;
+        }
+        int l = 0;
+        int r = 0;
+        int mini = INT_MAX;
+        int head = 0;
+        while(r<n){
+                if(mpt[s[r]] > 0){
+                    mps[s[r]]++;
+                    if(mps[s[r]] <= mpt[s[r]])count--;
+                    while(count == 0){
+                        if(r-l+1 < mini){
+                            mini = r-l+1;
+                            head = l;
+                        }
+                        if(mpt[s[l]] > 0) {
+                            mps[s[l]]--;
+                            if(mps[s[l]] < mpt[s[l]]) count++;
+                        }
+                        l++;
+                    }
+                }
+                r++;
+        }
+        return (mini == INT_MAX)? "" : s.substr(head, mini);
+    }
+};
