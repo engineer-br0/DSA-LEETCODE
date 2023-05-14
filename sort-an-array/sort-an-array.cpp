f6f5c43ce2a2f18c75cd0db353068b206179cec7
Mridul Sharma <106507655+engineer-br0@users.noreply.github.com>
2023-05-14 12:48:39 +0530

    Time: 580 ms (30.67%) | Memory: 144.8 MB (34.17%) - LeetSync

---
+++ sort-an-array/sort-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums, int l, int mid, int r){
+        int left = l;
+        int right = mid+1;
+        int final = 0;
+        vector<int> v(r - l +1, -1);
+        while(final <= r - l ){
+            if(left > mid) v[final++] = nums[right++];
+            else if(right > r) v[final++] = nums[left++];
+            else if(nums[right] < nums[left]) v[final++] = nums[right++];
+            else v[final++] = nums[left++];
+        }
+        for(int i : v) nums[l++] = i;
+    }
+    void mergesort(vector<int>& nums, int l, int r){
+        if(l >= r) return;
+        int mid = (l+r)/2;
+        mergesort(nums, l, mid);
+        mergesort(nums, mid+1, r);
+        merge(nums, l, mid, r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergesort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
