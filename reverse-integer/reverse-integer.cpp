762bc788beaaa129d49a9beacbc3994edb1985a8
Mridul Sharma <106507655+engineer-br0@users.noreply.github.com>
2023-05-21 01:49:24 +0530

    Time: 7 ms (17.16%) | Memory: 6 MB (59.58%) - LeetSync

--- reverse-integer/reverse-integer.cpp
+++ reverse-integer/reverse-integer.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        if(x == -2147483648) return 0;
-         int ans = 0;
-         int mul = 1;
-         if(x < 0) {
-             mul = -1;
-             x = -x;
-         }
-        
-        int limit = INT_MAX/10;
-        while(x){
-            if(ans > limit) return 0;
-            //else if(ans == limit )
-            ans = ans * 10 + x % 10;
-            x = x/10;
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
         }
-        
-        return ans * mul;
+        return rev;
     }
 };
